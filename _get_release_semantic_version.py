dcfb65e16df3bac3c86d7e65d66fc7d14b0b4e65
Carl Csaposs <carl.csaposs@canonical.com>
2023-04-20 11:34:57 +0000

    patch: Format _get_release_semantic_version.py (#39)

--- _get_release_semantic_version.py
+++ _get_release_semantic_version.py
@@ -6,18 +6,29 @@ import subprocess
 # Get last release version
 # Example: "foobar	Latest	v1.1.2	2023-02-15T17:50:06Z"
 last_release = subprocess.check_output(
-    ["gh", "release", "list", "--limit", "1", "--exclude-drafts", "--exclude-pre-releases"],
+    [
+        "gh",
+        "release",
+        "list",
+        "--limit",
+        "1",
+        "--exclude-drafts",
+        "--exclude-pre-releases",
+    ],
     encoding="utf-8",
 )
 last_release_version = last_release.split("\t")[2]
 assert re.fullmatch(
     r"v[0-9]+\.[0-9]+\.[0-9]+", last_release_version
 ), f"Invalid version format {last_release_version=}"
-major, minor, patch = (int(component) for component in last_release_version[1:].split("."))
+major, minor, patch = (
+    int(component) for component in last_release_version[1:].split(".")
+)
 
 # Get prefixes (see CONTRIBUTING.md) of commits since last release
 commits_since_last_release = subprocess.check_output(
-    ["git", "log", f"{last_release_version}..HEAD", "--pretty=format:%s"], encoding="utf-8"
+    ["git", "log", f"{last_release_version}..HEAD", "--pretty=format:%s"],
+    encoding="utf-8",
 ).splitlines()
 prefixes = set()
 for commit_subject in commits_since_last_release:
@@ -40,7 +51,9 @@ elif "compatible" in prefixes:
 elif "patch" in prefixes:
     patch += 1
 else:
-    raise ValueError("No commits since last release with valid prefix (see CONTRIBUTING.md).")
+    raise ValueError(
+        "No commits since last release with valid prefix (see CONTRIBUTING.md)."
+    )
 
 output = f"full_version=v{major}.{minor}.{patch}\nmajor_version=v{major}"
 print(output)
