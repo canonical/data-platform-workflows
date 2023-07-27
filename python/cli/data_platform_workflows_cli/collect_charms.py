a060f8596f35a0d3d1a249fbe7c55f7e1d1392ef
Carl Csaposs <carl.csaposs@canonical.com>
2023-07-27 13:46:03 +0000

    patch: Move Python scripts to CLI package (#62)
    Move python scripts into an installable Python package
    
    Benefits of Python package:
    - Easier to install than checking out repository
    - Dependencies (e.g. pyyaml, requests) specified by package & don't need
    to be installed separately
    - Code between scripts can be shared in the future
    
    This PR lays the groundwork for adding shared pytest plugins and for a
    reusable integration test workflow

--- python/cli/data_platform_workflows_cli/collect_charms.py
+++ python/cli/data_platform_workflows_cli/collect_charms.py
@@ -33,20 +33,21 @@ def get_base_versions(path_to_charmcraft_yaml: Path) -> list[str]:
     return versions
 
 
-logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-charms = []
-for charmcraft_yaml in Path(".").glob("**/charmcraft.yaml"):
-    path = charmcraft_yaml.parent
-    charm_name = yaml.safe_load((path / "metadata.yaml").read_text())["name"]
-    for index, version in enumerate(get_base_versions(charmcraft_yaml)):
-        charms.append(
-            {
-                "_job_display_name": f"Build {charm_name} charm | {version}",
-                "bases_index": index,
-                "directory_path": path.as_posix(),
-                "file_path": f"./{path/charm_name}_ubuntu-{version}-amd64.charm",
-            }
-        )
-logging.info(f"Collected {charms=}")
-with open(os.environ["GITHUB_OUTPUT"], "a") as file:
-    file.write(f"charms={json.dumps(charms)}")
+def main():
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    charms = []
+    for charmcraft_yaml in Path(".").glob("**/charmcraft.yaml"):
+        path = charmcraft_yaml.parent
+        charm_name = yaml.safe_load((path / "metadata.yaml").read_text())["name"]
+        for index, version in enumerate(get_base_versions(charmcraft_yaml)):
+            charms.append(
+                {
+                    "_job_display_name": f"Build {charm_name} charm | {version}",
+                    "bases_index": index,
+                    "directory_path": path.as_posix(),
+                    "file_path": f"./{path/charm_name}_ubuntu-{version}-amd64.charm",
+                }
+            )
+    logging.info(f"Collected {charms=}")
+    with open(os.environ["GITHUB_OUTPUT"], "a") as file:
+        file.write(f"charms={json.dumps(charms)}")
