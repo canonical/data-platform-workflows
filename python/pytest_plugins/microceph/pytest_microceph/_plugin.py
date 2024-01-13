9b294048196d4efb9f9eb8b9fe8410a31f8dfc22
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2024-01-13 19:51:08 +0100

    Add option and clean-up routine

--- python/pytest_plugins/microceph/pytest_microceph/_plugin.py
+++ python/pytest_plugins/microceph/pytest_microceph/_plugin.py
@@ -13,16 +13,25 @@ class ConnectionInformation:
     access_key_id: str
     secret_access_key: str
     bucket: str
+    keep_after_finished: bool
 
 
 @pytest.fixture(scope="session")
-def microceph():
+def microceph(request):
     if not os.environ.get("CI") == "true":
         raise Exception("Not running on CI. Skipping microceph installation")
+    if "microceph" in subprocess.check_output(
+        ["sudo", "snap", "list"]
+    ).decode():
+        logger.info("Microceph already set, leaving...")
+        return
     logger.info("Setting up microceph")
     subprocess.run(["sudo", "snap", "install", "microceph"], check=True)
     subprocess.run(["sudo", "microceph", "cluster", "bootstrap"], check=True)
-    subprocess.run(["sudo", "microceph", "disk", "add", "loop,4G,3"], check=True)
+    subprocess.run(
+        ["sudo", "microceph", "disk", "add", "loop,4G,3"],
+        check=True
+    )
     subprocess.run(["sudo", "microceph", "enable", "rgw"], check=True)
     output = subprocess.run(
         [
@@ -50,8 +59,39 @@ def microceph():
         aws_secret_access_key=secret_key,
     ).create_bucket(Bucket=_BUCKET)
     logger.info("Set up microceph")
-    return ConnectionInformation(key_id, secret_key, _BUCKET)
+    return ConnectionInformation(
+        key_id,
+        secret_key,
+        _BUCKET,
+        request.config.option.keep_microceph
+    )
 
 
 _BUCKET = "testbucket"
 logger = logging.getLogger(__name__)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--keep-microceph",
+        action="store_true",
+        help="Does not delete microceph at the end of the test.",
+    )
+
+
+@pytest.fixture(scope="session", autouse=True)
+def clean_microceph(microceph):
+    yield
+
+    if "microceph" not in subprocess.check_output(
+        ["sudo", "snap", "list"]
+    ).decode():
+        logger.info("Microceph not installed, leaving...")
+        return
+    if microceph.keep_after_finished:
+        logger.debug("Keeping microceph running as requested")
+        return
+    subprocess.run(
+        ["sudo", "snap", "remove", "--purge", "microceph"],
+        check=True
+    )
