ae1381c3f3d04aae8a7b5eab499274cd9ce8b291
Carl Csaposs <carl.csaposs@canonical.com>
2024-04-09 15:42:12 +0200

    Filter by model

--- python/cli/data_platform_workflows_cli/convert_logsink_to_debug_log.py
+++ python/cli/data_platform_workflows_cli/convert_logsink_to_debug_log.py
@@ -2,28 +2,24 @@
 
 import argparse
 import enum
+import json
 import pathlib
 import re
 import subprocess
 
 PATTERN = re.compile(
-    r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}: (?P<entity>.*?) [0-9]{4}-[0-9]{2}-[0-9]{2} (?P<time>[0-9]{2}:[0-9]{2}:[0-9]{2}) (?P<level>TRACE|DEBUG|INFO|WARNING|ERROR|CRITICAL) (?P<message>.*)\n"
+    r"(?P<model>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}): (?P<entity>.*?) [0-9]{4}-[0-9]{2}-[0-9]{2} (?P<time>[0-9]{2}:[0-9]{2}:[0-9]{2}) (?P<level>TRACE|DEBUG|INFO|WARNING|ERROR|CRITICAL) (?P<message>.*)\n"
 )
 LINE_NUMBER_PATTERN = re.compile(r" \S+\.go:[0-9]+ ")
 
 
 class Level(enum.Enum):
-    TRACE = (0, "[39m")
-    DEBUG = (1, "[32m")
-    INFO = (2, "[94m")
-    WARNING = (3, "[33m")
-    ERROR = (4, "[91m")
-    CRITICAL = (5, "[41;97m")
-
-    def __lt__(self, other):
-        if not isinstance(other, type(self)):
-            raise TypeError
-        return self.value[0] < other.value[0]
+    TRACE = "[39m"
+    DEBUG = "[32m"
+    INFO = "[94m"
+    WARNING = "[33m"
+    ERROR = "[91m"
+    CRITICAL = "[41;97m"
 
     def __str__(self):
         """Name with ANSI color code"""
@@ -47,6 +43,14 @@ def main():
     args = parser.parse_args()
     logsink = pathlib.Path(args.logsink_file).expanduser()
     output = pathlib.Path(args.output_file).expanduser()
+    model = json.loads(
+        subprocess.run(
+            ["juju", "show-model", "--format", "json"],
+            capture_output=True,
+            check=True,
+            encoding="utf-8",
+        ).stdout
+    )["test"]["controller-uuid"]
     with logsink.open("r", encoding="utf-8") as logsink_file, output.open(
         "w", encoding="utf-8"
     ) as output_file:
@@ -57,14 +61,8 @@ def main():
             if match:
                 # First line of log message
                 skip = False
-                entity = match.group("entity")
-                level = Level[match.group("level")]
-                if entity.startswith("unit-"):
-                    if level < Level.DEBUG:
-                        skip = True
-                else:
-                    if level < Level.INFO:
-                        skip = True
+                if match.group("model") != model:
+                    skip = True
                 # Example of original line:
                 # "276ce0fa-a69d-4e2c-8793-031a46bac9e1: machine-3 2024-04-05 14:50:47 INFO juju.worker.leadership tracker.go:194 opensearch/2 promoted to leadership of opensearch "
                 # Example of replaced line:
@@ -75,6 +73,6 @@ def main():
                     match.group("message").rstrip(" "),
                     count=1,
                 )
-                line = f'{entity}: {match.group("time")} {level} {message}\n'
+                line = f'{match.group("entity")}: {match.group("time")} {Level[match.group("level")]} {message}\n'
             if not skip:
                 output_file.write(line)
