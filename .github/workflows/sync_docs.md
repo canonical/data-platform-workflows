4c77e41a7ee4107b06187ebe8e6f29ac42dd1f41
Andreia Velasco <andreia.velasco@canonical.com>
2024-08-16 12:23:31 +0200

    compatible: Add sync_docs.yaml (#220)
    The sync_docs workflow that originally called the discourse-gatekeeper
    action was replaced by a small custom script.
    
    ## Reasoning
    - Greatly simplifies troubleshooting and maintenance of our
    documentation sync workflow.
    - Removes two important limitations:
    1. We can give navigation groups their own landing page (e.g. A level 1
    `[Tutorial]()` row can now have a landing page `[Tutorial](/t/123)`
    while remaining an expandable group). This functionality is part of
    Canonical's documentation requirements (see [Diataxis docs regarding
    navigation](https://diataxis.fr/complex-hierarchies/#structure-of-documentation-content)).
    2. We can nest items more than 1 level away from each other (e.g. A
    level 3 page can be followed by a level 1 page)
    
    ---------
    
    Co-authored-by: Carl Csaposs <carl.csaposs@canonical.com>

---
+++ .github/workflows/sync_docs.md
@@ -0,0 +1,95 @@
+Workflow file: [sync_docs.yaml](sync_docs.yaml)
+
+## Usage
+Add `.yaml` file to `.github/workflows/`
+
+```yaml
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+name: Sync Discourse docs
+
+on:
+  workflow_dispatch:
+  schedule:
+    - cron: '53 0 * * *'  # Daily at 00:53 UTC
+
+jobs:
+  sync-docs:
+    name: Sync docs from Discourse
+    uses: canonical/data-platform-workflows/.github/workflows/sync_docs.yaml@v0.0.0
+    with:
+      reviewers: canonical/data-platform-technical-authors,octocat
+    permissions:
+      contents: write  # Needed to push branch & tag
+      pull-requests: write  # Needed to create PR
+```
+
+## Behavior
+
+Downloads all Discourse topics in the charm's Charmhub documentation to `/docs/` directory in the charm's repository.
+
+>[!NOTE]
+> Any content in the `/docs/` directory that is not part of this workflow will get removed.
+
+The topics are determined by the navigation table in the charm's overview page - i.e. the page linked in the `metadata.yaml` `docs:` field. 
+
+When the workflow is triggered, it downloads all Discourse topics in their latest state and compares them to the `/docs/` directory in the default branch (e.g. `main`).
+* If the contents match, GitHub is up to date with Discourse. Nothing happens.
+* If the contents do not match, Discourse is ahead of GitHub. The workflow opens or updates a PR for the `sync-docs` branch with the diff between Discourse and GitHub.
+
+Each Discourse topic is downloaded to either: `/docs/`, `/docs/tutorial/`, `/docs/how-to/`, `/docs/reference/`, or `/docs/explanation/` depending on their slug prefix. Sub-categories will not create an additional directory.
+
+```
+# Example navtable:
+
+| Level |      Path      |          Navlink          |
+|-------|----------------|---------------------------|
+|   1   | t-landing      | [Tutorial](/t/123)        |
+|   2   | t-introduction | [Introduction](/t/124)    |
+|   1   | h-landing      | [How To]()                |
+|   2   | h-set-up       | [Set up](/t/125)          |
+|   3   | h-deploy-lxd   | [Deploy on LXD](/t/126)   |
+|   2   | h-monitoring   | [Monitoring]()            |
+|   3   | h-alert        | [Add alert rules](/t/127) |
+
+# Expected /docs output:
+
+/docs/overview.md
+/docs/tutorial/t-landing.md
+/docs/tutorial/t-introduction.md
+/docs/how-to/h-set-up.md
+/docs/how-to/h-deploy-lxd.md
+/docs/how-to/h-alert.md
+```
+
+## Documentation requirements
+
+**Requirement**: The overview topic must contain a navigation table with the columns "Level", "Path", and "Navlink"
+
+**Requirement**: The navigation table of the overview topic must be wrapped by the `[details]` HTML/markdown element as follows:
+
+```
+# Navigation
+
+[details=Navigation]
+
+<navigation table goes here>
+
+[/details]
+
+```
+
+> [!NOTE]  
+> Make sure to leave a white space before and after the `[details]` and `[/details]` lines. Otherwise, Discourse/Charmhub may not parse them correctly.
+
+**Requirement**: Links to topics in the `Navlink` column must be formatted as `[Text](/t/<topic_id>)`. 
+
+They **must not** be formatted as `[Text](/t/<some-additional-slug-text>/<topic_id>)`.
+
+### Examples
+The following documentation sets fulfill the above requirements and have been tested:
+* [PostgreSQL VM](https://discourse.charmhub.io/t/charmed-postgresql-documentation/9710)
+* [PostgreSQL K8s](https://discourse.charmhub.io/t/charmed-postgresql-k8s-documentation/9307)
+* [PgBouncer VM](https://discourse.charmhub.io/t/pgbouncer-documentation/12133)
+* [PgBouncer K8s](https://discourse.charmhub.io/t/pgbouncer-k8s-documentation/12132)
+* [MySQL Router K8s](https://discourse.charmhub.io/t/mysql-router-k8s-documentation/12130)
---
+++ python/cli/data_platform_workflows_cli/sync_docs.py
@@ -0,0 +1,145 @@
+import csv
+import dataclasses
+import logging
+import pathlib
+import re
+import shutil
+import sys
+
+import requests
+import yaml
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+DOCS_LOCAL_PATH = pathlib.Path("docs/")
+
+
+def get_topic(topic_id_: str):
+    """Get markdown content of a discourse.charmhub.io topic"""
+
+    response = requests.get(
+        f"https://discourse.charmhub.io/raw/{topic_id_}/1"
+    )  # "/1" for post 1
+
+    response.raise_for_status()
+    return response.text
+
+
+class NoTopicToDownload(Exception):
+    """No Discourse topic is available to download
+
+    Happens if:
+    - no "Navlink" is provided (e.g. for a navigation group)
+    - "Navlink" is an external URL
+    """
+
+
+@dataclasses.dataclass
+class Topic:
+    """Discourse topic to download"""
+
+    id: str
+    path: pathlib.Path
+
+    @classmethod
+    def from_csv_row(cls, row_: dict):
+        # Example `row_`: {'Level': '2', 'Path': 't-introduction', 'Navlink': '[Introduction](/t/9707)'}
+
+        # Extract Discourse topic ID from "Navlink"
+        # Example `link`: "/t/9707"
+        link = re.fullmatch(r"\[.*?]\((.*?)\)", row_["Navlink"]).group(1)
+        if link == "":
+            raise NoTopicToDownload
+        elif link.startswith("http") and "discourse.charmhub.io" not in link:
+            # Ignore external links (e.g. "https://canonical.com/data/docs/postgresql/iaas")
+            raise NoTopicToDownload
+
+        match = re.fullmatch(r"/t/([0-9]+)", link)
+        if not match:
+            raise ValueError(
+                f'Invalid navlink "{link}". Expected something like "/t/9707"'
+            )
+        # Example `topic_id`: "9707"
+        topic_id = match.group(1)
+
+        # Determine local path to download Markdown file
+        # Example `topic_slug`: "t-introduction"
+        topic_slug = row_["Path"]
+        diataxis_directory = {
+            "t-": "tutorial",
+            "h-": "how-to",
+            "r-": "reference",
+            "e-": "explanation",
+        }.get(topic_slug[:2], "")
+
+        # Example `path`: "docs/tutorial/t-introduction.md"
+        path = DOCS_LOCAL_PATH / diataxis_directory / f"{topic_slug}.md"
+
+        return cls(topic_id, path)
+
+    def download(self):
+        """Download topic markdown to path"""
+        self.path.parent.mkdir(parents=True, exist_ok=True)
+        self.path.write_text(get_topic(self.id))
+        logging.info(f"Downloaded {self=}")
+
+
+def main():
+    """Update Discourse documentation topics in docs/ directory"""
+
+    # Example `overview_topic_link`: "https://discourse.charmhub.io/t/charmed-postgresql-documentation/9710"
+    overview_topic_link: str = yaml.safe_load(
+        pathlib.Path("metadata.yaml").read_text()
+    )["docs"]
+    assert overview_topic_link.startswith("https://discourse.charmhub.io/")
+
+    # Example `overview_topic_id`: "9710"
+    overview_topic_id = overview_topic_link.split("/")[-1]
+    overview_topic_markdown = get_topic(overview_topic_id)
+
+    # Extract navigation table from Markdown
+    match = re.search(
+        r"\[details=Navigation]\n(.*?)\n\[/details]",
+        overview_topic_markdown,
+        flags=re.DOTALL,
+    )
+    if not match:
+        raise ValueError("Unable to find navigation table")
+
+    # Example `table`:
+    # | Level | Path | Navlink |
+    # |--------|--------|-------------|
+    # | 1 | tutorial | [Tutorial]() |
+    # | 2 | t-introduction | [Introduction](/t/9707) |
+    # | 2 | t-set-up | [1. Set up the environment](/t/9709) |
+    # | 2 | t-deploy | [2. Deploy PostgreSQL](/t/9697) |
+    # | 1 | search | [Search](https://canonical.com/data/docs/postgresql/iaas) |
+    table = match.group(1).strip()
+
+    # Convert Markdown table to list[dict[str, str]]
+    # (https://stackoverflow.com/a/78254495)
+    rows: list[dict] = list(csv.DictReader(table.split("\n"), delimiter="|"))
+    # Remove first row (e.g. "|--------|--------|-------------|")
+    rows = rows[1:]
+    rows: list[dict[str, str]] = [
+        {key.strip(): value.strip() for key, value in row.items() if key != ""}
+        for row in rows
+    ]
+
+    try:
+        shutil.rmtree(DOCS_LOCAL_PATH)
+        logging.info(f"Deleted {DOCS_LOCAL_PATH} directory")
+    except FileNotFoundError:
+        pass
+
+    # Download the overview topic since it's not part of the navtable
+    Topic(overview_topic_id, DOCS_LOCAL_PATH / "overview.md").download()
+
+    # Download topics in navtable
+    for row in rows:
+        # Example `row`: {'Level': '2', 'Path': 't-introduction', 'Navlink': '[Introduction](/t/9707)'}
+        try:
+            topic = Topic.from_csv_row(row)
+        except NoTopicToDownload:
+            continue
+
+        topic.download()
