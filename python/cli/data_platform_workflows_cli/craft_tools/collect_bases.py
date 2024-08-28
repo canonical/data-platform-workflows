06f252ea079edfd055cee236ede28c237467f9b0
Carl Csaposs <carl.csaposs@canonical.com>
2024-08-28 08:38:19 +0000

    patch(build_*.yaml): Switch ARM runner image (#233)
    On September 3rd, the GitHub Ubuntu arm64 image will be removed.
    (https://github.blog/changelog/2024-08-19-notice-of-upcoming-deprecations-and-breaking-changes-in-github-actions-runners/)
    
    Switch to runner with "Ubuntu 22.04 by Arm Limited" image

--- python/cli/data_platform_workflows_cli/craft_tools/collect_bases.py
+++ python/cli/data_platform_workflows_cli/craft_tools/collect_bases.py
@@ -24,7 +24,7 @@ from . import craft
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 RUNNERS = {
     craft.Architecture.X64: "ubuntu-latest",
-    craft.Architecture.ARM64: "Ubuntu_ARM64_4C_16G_01",
+    craft.Architecture.ARM64: "Ubuntu_ARM64_4C_16G_02",
 }
 
 
