df95aa423efea58aaff4691be5c542ff5cd1fc8e
Lucas Gameiro Borges <lucas.borges@canonical.com>
2024-09-12 14:02:00 -0300

    use channel 3.x/stable for charmcraft

--- python/cli/data_platform_workflows_cli/parse_snap_version.py
+++ python/cli/data_platform_workflows_cli/parse_snap_version.py
@@ -18,7 +18,7 @@ def main():
     elif args.channel:
         install_flag = f"'--channel={args.channel}'"
     elif args.channel_input_name == "charmcraft-snap-channel":
-        install_flag = "'--channel=2.x/stable'"
+        install_flag = "'--channel=3.x/stable'"
     else:
         install_flag = None
     github_actions.output["install_flag"] = install_flag
