52c949d1ee5f43c4414cde0cbf289d3ba0100ae7
Carl Csaposs <carl.csaposs@canonical.com>
2026-02-16 11:07:13 +0000

    breaking(build_*.yaml): Replace *-snap-revision input with *-snap-revisions (#340)
    Different architectures require different snap revisions

--- _cli/data_platform_workflows_cli/parse_snap_version.py
+++ _cli/data_platform_workflows_cli/parse_snap_version.py
@@ -1,20 +1,62 @@
 import argparse
+import json
+import subprocess
 
 from . import github_actions
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--revision", required=True)
+    parser.add_argument("--revisions")
+    parser.add_argument("--revision")
     parser.add_argument("--channel", required=True)
     parser.add_argument("--revision-input-name", required=True)
     parser.add_argument("--channel-input-name", required=True)
     args = parser.parse_args()
-    if args.revision:
-        assert (
-            not args.channel
-        ), f"`{args.channel_input_name}` input cannot be used if `{args.revision_input_name}` input is passed"
-        install_flag = f"'--revision={args.revision}'"
+    # Validate workflow usage (not user input to workflow)
+    if args.revisions is not None and args.revision is not None:
+        raise ValueError("Only one of `--revisions` or `--revision` can be used")
+    elif args.revisions is None and args.revision is None:
+        raise ValueError("`--revisions` or `--revision` is required")
+
+    # Validate user input to workflow
+    if args.revisions is not None:
+        if args.revisions:
+            invalid_type_message = (
+                f"`{args.revision_input_name}` input must be JSON string with type "
+                f"dict[str, str], got {repr(args.revisions)}"
+            )
+            try:
+                revisions = json.loads(args.revisions)
+            except ValueError:
+                raise ValueError(invalid_type_message)
+            if not isinstance(revisions, dict):
+                raise ValueError(invalid_type_message)
+            if len(revisions) == 0:
+                raise ValueError(f"`{args.revision_input_name}` input must not be empty dict")
+            for key, value in revisions.items():
+                if not (isinstance(key, str) and isinstance(value, str)):
+                    raise ValueError(invalid_type_message)
+
+            architecture = subprocess.run(
+                ["dpkg", "--print-architecture"], capture_output=True, check=True, encoding="utf-8"
+            ).stdout.strip()
+            try:
+                revision = revisions[architecture]
+            except KeyError:
+                raise KeyError(
+                    f"{repr(architecture)} key missing from `{args.revision_input_name}` input"
+                )
+        else:
+            revision = None
+    else:
+        revision = args.revision
+    if revision:
+        assert not args.channel, (
+            f"`{args.channel_input_name}` input cannot be used if `{args.revision_input_name}` "
+            "input is passed"
+        )
+        install_flag = f"'--revision={revision}'"
     elif args.channel:
         install_flag = f"'--channel={args.channel}'"
     else:
