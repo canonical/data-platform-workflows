d3257d62e0f052a874615649696d6fc1c47c2296
Sinclert Pérez <sinclert.perez@canonical.com>
2026-04-15 13:01:40 +0200

    Remove workflow trigger check [revert]
    This reverts commit 0cae5c745c7310f745af3a8dfe65c8da40790e6d.

--- _cli/data_platform_workflows_cli/craft_tools/promote_legacy.py
+++ _cli/data_platform_workflows_cli/craft_tools/promote_legacy.py
@@ -161,6 +161,12 @@ def charm():
             f"{repr(from_risk.value)} to 'candidate' first"
         )
 
+    ref = args.ref
+    if not ref.startswith("refs/heads/"):
+        raise ValueError(
+            "This workflow must be run on `workflow_dispatch` from the branch that contains track "
+            f"{repr(track)}"
+        )
     default_branch = args.default_branch
 
     if not pathlib.Path(".github/release.yaml").exists():
